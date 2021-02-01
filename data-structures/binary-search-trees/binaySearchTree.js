22cdf79b3c1ff2731cf2a5e274fd8472165ec8c4
DESKTOP-SNOBUB8\ilker <ilkerkaanadiguzel@gmail.com>
2021-02-01 04:40:52 +0300

    studied binary search trees

---
+++ data-structures/binary-search-trees/binaySearchTree.js
@@ -0,0 +1,89 @@
+// Singly Linked List
+// Time Complexity
+// 	Insertion: O(logn) || Removal: O(log n)
+//  Space Complexity O(n)
+
+class Node {
+	constructor(value) {
+		// this.count = 1;
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.length = 0;
+		this.root = null;
+	}
+
+	insert(value) {
+		const newNode = new Node(value);
+
+		let currentNode = this.root;
+		this.length++;
+
+		if (!this.root) {
+			this.root = newNode;
+			return newNode;
+		} else {
+			while (true) {
+				// We can keep count how many times a number added to the tree or just return null;
+				// if (newNode.value === currentNode.value) {
+				// 	currentNode.count++;
+				// 	return currentNode;
+				// }
+				if (newNode.value === currentNode.value) return null;
+				if (newNode.value < currentNode.value) {
+					if (!currentNode.left) {
+						currentNode.left = newNode;
+						return newNode;
+					} else {
+						currentNode = currentNode.left;
+					}
+				} else if (newNode.value > currentNode.value) {
+					if (!currentNode.right) {
+						currentNode.right = newNode;
+						return newNode;
+					} else {
+						currentNode = currentNode.right;
+					}
+				}
+			}
+		}
+	}
+
+	find(searchValue) {
+		if (!this.root) {
+			return false;
+		}
+
+		let currentNode = this.root;
+		let found = false;
+		while (currentNode && !found) {
+			if (searchValue < currentNode.value) {
+				currentNode = currentNode.left;
+			} else if (searchValue > currentNode.value) {
+				currentNode = currentNode.right;
+			} else {
+				found = true;
+			}
+		}
+
+		return found && currentNode;
+	}
+}
+
+let bst = new BinarySearchTree();
+
+bst.insert(4);
+bst.insert(2);
+bst.insert(-1);
+bst.insert(1);
+bst.insert(11);
+bst.insert(7);
+bst.insert(26);
+bst.insert(13);
+bst.insert(26);
+bst.insert(5);

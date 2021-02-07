25a756a2a92621ace6c87d2b50119223ff5dae33
DESKTOP-SNOBUB8\ilker <ilkerkaanadiguzel@gmail.com>
2021-02-07 18:57:37 +0300

    implemented bfs and dfs on bst

--- data-structures/binary-search-trees/binaySearchTree.js
+++ data-structures/binary-search-trees/binaySearchTree.js
@@ -73,6 +73,77 @@ class BinarySearchTree {
 
 		return found && currentNode;
 	}
+
+	BFS() {
+		let node = this.root,
+			data = [],
+			queue = [];
+
+		queue.push(node);
+
+		while (queue.length) {
+			node = queue.shift();
+			data.push(node);
+
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+
+		return data;
+	}
+
+	DFSPreOrder() {
+		let data = [];
+
+		const traverse = (node) => {
+			data.push(node);
+
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+		};
+
+		traverse(this.root);
+
+		return data;
+	}
+
+	DFSPostOrder() {
+		let data = [];
+
+		const traverse = (node) => {
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+
+			data.push(node);
+		};
+
+		traverse(this.root);
+
+		return data;
+	}
+
+	DFSInOrder() {
+		let data = [];
+
+		const traverse = (node) => {
+			if (node.left) traverse(node.left);
+			data.push(node);
+			if (node.right) traverse(node.right);
+		};
+
+		traverse(this.root);
+
+		return data;
+	}
 }
 
-let bst = new BinarySearchTree();
+const x = new BinarySearchTree();
+
+x.insert(1);
+x.insert(7);
+x.insert(-3);
+x.insert(6);
+x.insert(4);
+x.insert(9);
+x.insert(15);
+x.insert(2);
